bc1d3cfaf21ff1f3466d6e679f259c1c0d370334
Cory Barton <coryjbarton@gmail.com>
2018-10-12 17:09:41 -0600

    Added Pods

--- Tappy Phone/AppDelegate.swift
+++ Tappy Phone/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        GADMobileAds.configure(withApplicationID: "ca-app-pub-8758014690935874~9118365092")
+        
         return true
     }
 
