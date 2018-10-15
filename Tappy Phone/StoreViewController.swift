57960bf220bdf5f2ecde973f1d36c57af53e80e8
Cory Barton <coryjbarton@gmail.com>
2018-10-15 09:25:50 -0600

    Add in app purchases

--- Tappy Phone/StoreViewController.swift
+++ Tappy Phone/StoreViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import StoreKit
 
-class StoreViewController: UIViewController {
-
+class StoreViewController: UIViewController, SKPaymentTransactionObserver, SKProductsRequestDelegate {
+    
     @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var button1: UIButton!
     
@@ -18,6 +19,9 @@ class StoreViewController: UIViewController {
     @IBOutlet weak var productTitle: UILabel!
     @IBOutlet weak var productDescription: UITextView!
     
+    var product: SKProduct?
+    var productID = "com.corybarton.tappyphone.removeads"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,7 +29,11 @@ class StoreViewController: UIViewController {
         button1.layer.cornerRadius = 5.0
         buyButton.layer.cornerRadius = 5.0
         
-
+        buyButton.isEnabled = false
+        SKPaymentQueue.default().add(self)
+        getPurchaseInfo()
+        
+        
         // Do any additional setup after loading the view.
     }
     
@@ -36,6 +44,83 @@ class StoreViewController: UIViewController {
     }
     
     @IBAction func purchase(_ sender: Any) {
+        
+        let payment = SKPayment(product: product!)
+        SKPaymentQueue.default().add(payment)
+        
+    }
+    
+    func getPurchaseInfo() {
+        
+        if SKPaymentQueue.canMakePayments() {
+            
+            let request = SKProductsRequest(productIdentifiers: NSSet(objects: self.productID) as! Set<String>)
+            
+            request.delegate = self
+            request.start()
+            
+        } else {
+            
+            productTitle.text = "Warning"
+            productDescription.text = "Please enable in app purchases in your settings"
+            
+        }
+        
+    }
+    
+    func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+        
+        var products = response.products
+        
+        if (products.count == 0) {
+            
+            productTitle.text = "Warning"
+            productDescription.text = "Product Not Found!"
+            
+        } else {
+            
+            product = products[0]
+            productTitle.text = product!.localizedTitle
+            productDescription.text = product!.localizedDescription
+            buyButton.isEnabled = true
+            
+        }
+        
+        let invalids = response.invalidProductIdentifiers
+        
+        for product in invalids {
+            
+            print("product not found \(product)")
+            
+        }
+        
+    }
+    
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        
+        for transaction in transactions {
+            
+            switch transaction.transactionState {
+            case SKPaymentTransactionState.purchased:
+                
+                SKPaymentQueue.default().finishTransaction(transaction)
+                productTitle.text = "Thank you"
+                productDescription.text = "The product has been purchased"
+                buyButton.isEnabled = false
+                
+            case SKPaymentTransactionState.failed:
+                
+                SKPaymentQueue.default().finishTransaction(transaction)
+                productTitle.text = "Warning"
+                productDescription.text = "The product has not been purchased"
+                
+            default:
+                break
+                
+            }
+            
+        }
+        
     }
     
 }
